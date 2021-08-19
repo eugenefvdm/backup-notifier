5f4c95aeb9b316bd18ad43f5f54a7e49182f9847
Eugene <eugenevdm@gmail.com>
2021-08-19 05:40:02 +0200

    3rd commit

--- notify-slack.sh
+++ notify-slack.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 # Example
-# sh /root/backup-report.sh superman $BACKUP_DEST $BACKUP_STATUS
 
-status=$3
-destination=$2
-message="$1 backup report for $2 is $3"
+# Command to run before backup
+# sh /root/backup-report.sh Begin Superman $BACKUP_DEST
+
+# Command to run after backup
+# sh /root/backup-report.sh End Superman $BACKUP_DEST $BACKUP_STATUS
+
+# Command line parameters
+verb=$1
+server=$2
+destination=$3
+status=$4
+
+message="$1 $2 backup  $3 $4"
 echo $message
 
 # Read the Slack Hook URL from the .env file
@@ -13,4 +22,3 @@ source /root/.env
 
 # Post to Slack
 curl -X POST -H 'Content-type: application/json' --data "{'text':'$message'}" $SLACK_HOOK
-
