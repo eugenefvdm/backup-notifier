35c01812a40ee7ba57e3f8a0268034cf7c5c3817
Eugene <eugenevdm@gmail.com>
2021-08-21 12:00:09 +0200

    success / fail intepretation and slack emojis

--- notify-slack.sh
+++ notify-slack.sh
@@ -23,21 +23,24 @@ status=$4
 message="$1 $2 backup to $3 $4"
 echo $message
 
-if [ $1 = Start ]
-then
+if [ $1 = Start ] ; then
     gawk 'BEGIN { print "START=" systime() }' > $directory/notify-slack-start-marker.start
 fi
 
-if [ $1 = End ]
-then
+if [ $1 = End ] ; then
     gawk 'BEGIN { print "END=" systime() }' > $directory/notify-slack-start-marker.end
+    # Source the start and end markers
     . $directory/notify-slack-start-marker.start
     . $directory/notify-slack-start-marker.end    
-    echo "The backup took $((($END - $START) / 60)) minutes and $((($END - $START) % 60)) seconds"
+    duration="$((($END - $START) / 60)) minutes and $((($END - $START) % 60)) seconds"
+    if [ $status = 1 ] ; then
+        message=":smile: $2 backup to $3 was a success and took $duration";        
+    else
+        message=":face_with_symbols_on_mouth: $2 backup to $3 FAILED and took $duration";
+    fi
 fi
 
-if [ $APP_DEBUG = false ]
-then
-    # Post to Slack
+# Post to Slack when the app is not in debug mode
+if [ $APP_DEBUG = false ] ; then    
     curl -X POST -H 'Content-type: application/json' --data "{'text':'$message'}" $SLACK_HOOK
 fi
