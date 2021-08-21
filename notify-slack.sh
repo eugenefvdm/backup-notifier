ec82894120c426a78021baceb3b6b08f33cab5e4
Eugene <eugenevdm@gmail.com>
2021-08-21 11:24:00 +0200

    fix

--- notify-slack.sh
+++ notify-slack.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+directory="$(dirname "$(realpath "$0")")"
+
+#directory=/home/eugene/code/virtualmin-slack-backup-notification
+
 # Example Use
 # verb server destination status
-
 
 # Command to run before backup
 # sh /root/notify-slack.sh Start Superman $BACKUP_DEST
@@ -11,8 +14,15 @@
 # sh /root/notify-slack.sh End Superman $BACKUP_DEST $BACKUP_STATUS
 
 # Variables
-#directory=/home/eugene/code/virtualmin-slack-backup-notification
-directory=/root
+# directory=/root
+
+# Source the environment: SLACK_HOOK, APP_ENV, APP_DIRECTORY
+. $directory/.env
+
+#if [$APP_ENV = 'local']
+#then
+    #directory=$APP_DIRECTORY
+#fi
 
 # Command line parameters
 verb=$1
@@ -32,13 +42,9 @@ if [ $1 = End ]
 then
     gawk 'BEGIN { print "END=" systime() }' > $directory/notify-slack-start-marker.end
     . $directory/notify-slack-start-marker.start
-    . $directory/notify-slack-start-marker.end
-    thetime=$START-$END
+    . $directory/notify-slack-start-marker.end    
     echo "The backup took $((($END - $START) / 60)) minutes and $((($END - $START) % 60)) seconds"
 fi
-
-# Read the Slack Hook URL from the .env file
-. $directory/.env
 
 # Post to Slack
 # curl -X POST -H 'Content-type: application/json' --data "{'text':'$message'}" $SLACK_HOOK
